0d6e78ea50113fb57f6521b38a496083ba8d7f14
Moises Bumba <moises-alberto@hotmail.com>
2022-12-08 17:41:29 +0100

    cps minint project

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Moises Bumba',
+            'email' => 'moises-alberto@hotmail.com',
+            'password' => bcrypt('123456'),
+
+        ]);
+        
+    }
+}
